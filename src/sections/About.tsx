b96ecf7bfc21b59cc94788fa762521620d294cd3
Niteshpanday05 <eagleseye.np@gmail.com>
2025-12-30 20:03:37 +0545

    implementing reusable component in card header

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -11,7 +11,8 @@ import GithubIcon from "@/assets/icons/github.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png";
-import smileMemoji from '@/assets/images/memoji-smile.png';
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/CardHeader";
 
 const toolboxItems = [
   {
@@ -42,77 +43,85 @@ const toolboxItems = [
 
 const hobbies = [
   {
-    title:'Painting',
-    emoji:'🎨'
-  },
-   {
-    title:'Camera',
-    emoji:'📷'
-  },
-   {
-    title:'Footbal',
-    emoji:'⚽'
-  },
-   {
-    title:'Reading',
-    emoji:'📖'
+    title: "Painting",
+    emoji: "🎨",
   },
   {
-    title:'Watching',
-    emoji:'🎬'
+    title: "Camera",
+    emoji: "📷",
   },
   {
-    title:'Learning',
-    emoji:'🏫'
+    title: "Footbal",
+    emoji: "⚽",
+  },
+  {
+    title: "Reading",
+    emoji: "📖",
+  },
+  {
+    title: "Music",
+    emoji: "🎬",
+  },
+  {
+    title: "Hiking",
+    emoji: "🥾",
   },
 ];
 
 export const AboutSection = () => {
   return (
-    <div className="pb-96">
-      <SectionHeader
-        eyebrow="About Me"
-        title="A Glimse Into My World"
-        description="Learn more about who i am, what I do, and what inspires me"
-      />
-      <div>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Reads</h3>
-            <p>Explore the books shaping my perspectives</p>
-          </div>
-          <Image src={bookImage} alt="Book Cover" />
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>Toolbox</h3>
-            <p>
-              Explore the technologies and tools I use to create digital
-              experience
-            </p>
-          </div>
-          <div>
-            {toolboxItems.map((item) => (
-              <div key={item.title}>
-                <TechIcon component={item.iconType} />
-                <span>{item.title}</span>
-              </div>
-            ))}
-          </div>
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>Beyond the code</h3>
-            <p>Explore my interest and hobbies beyond the digital worl</p>
-          </div>
-        </Card>
-        <Card>
-          <Image src={mapImage} alt="map" />
-          <Image src={smileMemoji} alt="smiling memoji" />
-        </Card>
+    <div className="pb-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimse Into My World"
+          description="Learn more about who i am, what I do, and what inspires me"
+        />
+        <div className="mt-20 ">
+          <Card className="h-[320px]">
+            <CardHeader
+              title="My Reads"
+              description=" Explore the books shaping my perspectives"
+            />
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="Book Cover" className="" />
+            </div>
+          </Card>
+          <Card>
+            <CardHeader
+              title=" MyToolbox"
+              description=" Explore the technologies and tools I use to create digital
+                experience"
+            />
+            <div>
+              {toolboxItems.map((item) => (
+                <div key={item.title}>
+                  <TechIcon component={item.iconType} />
+                  <span>{item.title}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <CardHeader
+              title="Beyond the code"
+              description=" Explore my interest and hobbies beyond the digital worl"
+            />
+
+            <div>
+              {hobbies.map((hobby) => (
+                <div key={hobby.title}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="smiling memoji" />
+          </Card>
+        </div>
       </div>
     </div>
   );
