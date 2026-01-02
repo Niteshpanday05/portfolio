5663447fd0a3a59219118d75b49ab5c01c157166
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-02 21:29:33 +0545

    animation on project section done

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -59,13 +59,15 @@ export const ProjectsSection = () => {
         />
 
         <div className=" flex flex-col mt-10 gap-20 md:mt-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
               className=" px-8 pt-8 md:pt-12 md:px-10 
-               lg:px-20 lg:pt-16 pb-0  "
+               lg:px-20 lg:pt-16 pb-0 sticky top-16 "
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`,
+              }}
             >
-             
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-10">
                   <div className=" bg-gradient-to-r from-emerald-300 to-sky-400   inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
