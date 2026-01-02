86ccae585b861acfe9dbe9901d2361b317a499d1
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-02 18:16:53 +0545

    about section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
+import { ContactSection } from "@/sections/Contact";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <TapeSection />
       <TestimonialsSection />
       <AboutSection/>
+      <ContactSection/>
       <Footer/>
     </div>
   );
