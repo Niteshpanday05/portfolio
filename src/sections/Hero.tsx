b0a54e622cc13eb521e846915db39564bec08cf2
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-07 14:42:08 +0545

    some changes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -173,7 +173,7 @@ export const HeroSection = () => {
             className="inline-flex items-center  gap-2 bg-white border border-white text-gray-900 h-12 px-6 rounded-xl"
           >
             <span className="">👋</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">{"Let's Connect"}</span>
           </Link>
         </div>
       </div>
