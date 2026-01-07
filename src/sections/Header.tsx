16ebfb0aec0b219c7b079bb3ad899d68947f701f
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-07 11:05:49 +0545

    adjusting mobile view

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Header = () => {
  return(
    <div className="sticky top-3 z-50 flex justify-center items-center w-full">
-      <nav className="flex gap-6 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <nav className="flex gap-4 md:gap-6 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         
         <Link href="/" className="nav-item">
           Home
